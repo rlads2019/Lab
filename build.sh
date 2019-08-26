eb76e69856d0f1b8fc89a3d788a9606c9232534e
liao <liao961120@gmail.com>
2019-08-26 13:09:27 +0800

    use full directory

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ for dir in */
 do
     dir=${dir%*/}
     [[ ${dir} == 'src' || ${dir} == 'notes' ]] && continue
-    zip -r src/${dir:0:2}.zip ${dir}/R
+    zip -r src/${dir:0:2}.zip ${dir}
     cp ${dir}/notes.html notes/${dir:0:2}.html
 done
