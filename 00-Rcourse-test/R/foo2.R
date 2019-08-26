bf8e83f33bc1107d45c59be1e38dc529b57aad58
liao <liao961120@gmail.com>
2019-08-26 12:43:11 +0800

    first commit

---
+++ 00-Rcourse-test/R/foo2.R
@@ -0,0 +1,5 @@
+x = 1
+print(paste0('x is ', x))
+
+x = c(1, 2)
+print(paste0('x is ', x))
---
+++ build.sh
@@ -0,0 +1,12 @@
+[[ -d src ]] && rm -r src
+[[ -d notes ]] && rm -r notes
+mkdir src
+mkdir notes
+
+for dir in */
+do
+    dir=${dir%*/}
+    [[ ${dir} == 'src' || ${dir} == 'notes' ]] && continue
+    zip -r src/${dir:0:2}.zip ${dir}/R
+    cp ${dir}/notes.html notes/${dir:0:2}.html
+done
