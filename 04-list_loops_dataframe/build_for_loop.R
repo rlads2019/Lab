18abb77632756f9cebf8113bf146dcf8a79940a9
liao <liao961120@gmail.com>
2019-10-01 22:10:11 +0800

    update

---
+++ 04-list_loops_dataframe/build_for_loop.R
@@ -0,0 +1,7 @@
+member <- list(
+    kai = list(age = 40, about = "a professor"),
+    jessy = list(age = 20, about = "a cat lover"),
+    joy = list(age = 18, about = "a hacker"),
+    ben = list(age = 19, about = "a YouTuber")
+)
+
