6d3151937fd89691a739f329ed2604b263cb0a24
liao <liao961120@gmail.com>
2019-11-09 22:15:11 +0800

    update

--- 10-chinese_preprocess/segment.R
+++ 10-chinese_preprocess/segment.R
@@ -1,21 +1,19 @@
 library(jiebaR)
 library(stringr)
-library(quanteda)
-source("helper_func.R")
 
 fps <- list.files("samesex_marriage/txt", full.names = T)
 
 titles <- vector("character", length(fps))
 topics <- vector("character", length(fps))
 contents <- vector("character", length(fps))
+ids <- vector("character", length(fps))
 
-# Initialize jieba
-seg <- worker(bylines = F, symbol = T)
+# Initialize jiebaR
+seg <- worker(user = "samesex_marriage/user_dict.txt")
 
 for (i in seq_along(fps)) {
   # Get topic from file name
   topics[i] <- str_split(basename(fps[i]), "_")[[1]][1]
-  id[i] <- str_split(basename(fps[i]), fixed("."))[[1]][1]
   
   # Read post from file
   post <- readLines(fps[i])
@@ -25,15 +23,19 @@ for (i in seq_along(fps)) {
     
   # Segment post
   segged <- segment(post[-(1:2)], seg)
-  contents[i] <- segged %>% trimws() %>% paste(collapse = " ")
+  contents[i] <- paste(segged, collapse = " ")
 }
 
 
 # Combine results in a df
-df <- tibble::tibble(id = id, title = titles, topic = topics, content = contents)
+df <- tibble::tibble(id = seq_along(titles), topic = topics, title = titles, content = contents)
 #print(df)
+saveRDS(df, "samesex_marriage.rds")
 
 
+## quanteda framework
+library(quanteda)
+source("helper_func.R")
 qcorp <- df2qcorp(df, docid_colname = "id" , text_colname = "content")
 qcorp_tokens <- tokens(qcorp, "fastestword")
 x = kwic(qcorp_tokens, "同志", window = 7, valuetype = "regex")
@@ -43,3 +45,10 @@ kwic(qcorp_tokens, phrase("同志 圈")) %>% View()
 
 textstat_collocations(qcorp_tokens)
 tokens_select(qcorp_tokens, )
+
+
+## tidytext framework
+library(tidytext)
+tokens <- df %>% 
+  unnest_tokens(output = word, input = content,
+                token = "regex", pattern = " ")
