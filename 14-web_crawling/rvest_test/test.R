8d699ba89d105c8ba5698368895ab126ceaeb627
liao <liao961120@gmail.com>
2019-12-07 22:15:26 +0800

    update

--- 14-web_crawling/rvest_test/test.R
+++ 14-web_crawling/rvest_test/test.R
@@ -1,10 +1,19 @@
+library(httr)
 library(rvest)
 # servr::httw(port = 8888, daemon = TRUE)
 # servr::httd(port = 8888, daemon = TRUE)
-url = 'http://127.0.0.1:4000/'
-url =  'http://localhost:4000/'
+url = 'http://rlads2019.github.io/'
 req = httr::GET(url)
+req[["status_code"]]
+html = httr::content(req)
+html %>% html_nodes("a") %>% html_attr("href")
+html %>% html_nodes("a#forkme_banner") %>% html_attr("href")
+
+
 
 httr::content(req) %>% html_nodes(css = "ul li")
 
 read_html(url) %>% html_nodes(css = "ul, ol")
+
+
+rlads = read_html("")
